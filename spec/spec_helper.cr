02232c2625e755a1113fa0d58101dd38b96e763c
Hamdi Akoğuz <hamdiakoguz@phonoclick.com>
2016-04-20 00:30:41 +0300

    Move test log to spec.log file instead of STDOUT.

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,7 +2,7 @@ require "spec"
 require "../src/isot"
 require "./support/fixture"
 
-logger = Logger.new(STDOUT)
+logger = Logger.new(File.new("spec.log", "a"))
 logger.level = Logger::DEBUG
 Isot.logger = logger
 
