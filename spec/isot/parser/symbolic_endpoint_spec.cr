87f67053aef6c0af28c7013da7614f11f5cf33fd
Hamdi Akoğuz <hamdiakoguz@phonoclick.com>
2016-04-29 00:58:48 +0300

    Remove symbolic endpoint test.
    Wasabi spec was trying to make sure it does not raise exception for symbolic
    endpoints. Crystal URI class works a little bit differently than ruby's and
    parses them with no problem. The other test will already ensure it does not
    raise exception.

--- spec/isot/parser/symbolic_endpoint_spec.cr
+++ spec/isot/parser/symbolic_endpoint_spec.cr
@@ -5,10 +5,6 @@ describe Isot::Parser do
 
     subject = fixture(:symbolic_endpoint).parse
 
-    it "allows symbolic endpoints" do
-      subject.endpoint.should be_nil
-    end
-
     # it "should position base class attributes before subclass attributes in :order! array" do
     #   type = subject.types["ROPtsLiesListe"]
     #   type[:order!].should eq(["messages", "returncode", "listenteil"])
